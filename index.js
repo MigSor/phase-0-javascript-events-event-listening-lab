c83b24c826e9772ea3191664ed5e3b76c59c67d4
Miguel Soriano <miguel.sor.88@gmail.com>
2023-07-16 12:36:15 +1000

    make the button call an alert when pressed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const input = document.getElementById("button");
 
 function addingEventListener() {
-  input.addEventListener("click", function () {
-    alert("I was clicked");
-  });
+  input.addEventListener("click", clickAlert);
 }
+
+function clickAlert() {
+  alert("I was clicked!");
+}
+
+addingEventListener();
